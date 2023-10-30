bb38a9b2820bf79822e469a3e9d95cfd6cc3258d
Julius van Voorden <julius@vanvoorden.dev>
2023-10-30 20:09:08 +0100

    remove cheating remark

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,10 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-alert(
-  "Hey! Cheating is bad, you are a bad person. But thanks for using my software anyways, I appreciate it 😉\n~Julius"
-);
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
